46e8f738246540e34bc6e763ddd44cb0321db607
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-04-05 13:24:12 +0200

    Update for munch

--- components/EmailForm.tsx
+++ components/EmailForm.tsx
@@ -4,7 +4,12 @@ import { useState } from "react";
 import { toast } from "react-hot-toast";
 
 export default function EmailForm() {
+  const [name, setName] = useState<string>();
   const [email, setEmail] = useState<string>();
+
+  const handleNameChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setName(e.target.value);
+  };
 
   const handleEmailChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setEmail(e.target.value);
@@ -19,17 +24,20 @@ export default function EmailForm() {
         headers: {
           "Content-Type": "application/json",
         },
-        body: JSON.stringify({ email }),
+        body: JSON.stringify({ name, email }),
       });
 
       if (response.ok) {
+        setName("");
         setEmail("");
         toast.success("Thank you for joining our waitlist! 🚀");
       } else {
+        setName("");
         setEmail("");
         toast.error("Oops! Something went wrong!");
       }
     } catch (err) {
+      setName("");
       setEmail("");
       console.error(err);
     }
@@ -37,7 +45,22 @@ export default function EmailForm() {
   return (
     <>
       <form onSubmit={handleSubmit} method="POST" className="mt-2 max-w-sm">
-        <div className="flex flex-col gap-2 lg:flex-row">
+        <div className="flex flex-col gap-2">
+          <label className="sr-only" htmlFor="name">
+            Name
+          </label>
+          <input
+            autoComplete="name"
+            className="text-accent-500 block h-10 w-full focus:invalid:border-red-400 focus:invalid:text-red-500 focus:invalid:ring-red-500 appearance-none rounded-lg border-2 border-slate-300 px-4 py-2 placeholder-zinc-400 duration-200 focus:outline-none focus:ring-zinc-300 sm:text-sm"
+            pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
+            id="name"
+            name="name"
+            placeholder="John Doe"
+            required
+            type="name"
+            value={name}
+            onChange={handleNameChange}
+          />
           <label className="sr-only" htmlFor="email-address">
             Email address
           </label>
@@ -54,10 +77,10 @@ export default function EmailForm() {
             onChange={handleEmailChange}
           />
           <button
-            className="flex h-10 shrink-0 items-center justify-center gap-1 rounded-lg bg-[#000F2D] px-4 py-2 text-sm font-semibold text-white transition-all hover:bg-zinc-700"
+            className="flex h-10 shrink-0 items-center justify-center gap-1 rounded-lg bg-orange-500 px-4 py-2 text-sm font-semibold text-white transition-all hover:bg-orange-700"
             type="submit"
           >
-            <span>Join the waitlist</span>
+            <span>I'm interested!</span>
           </button>
         </div>
       </form>
@@ -65,8 +88,8 @@ export default function EmailForm() {
       <div className="flex items-start gap-2 text-gray-500">
         <InfoCircledIcon />
         <p className="text-xs -mt-[0.5] max-w-sm">
-          No worries! your data is completely safe and will only be utilized to
-          provide you with updates about our product.
+          No worries! your data is completely safe and will only be utilised to provide you with updates about our
+          product.
         </p>
       </div>
     </>
