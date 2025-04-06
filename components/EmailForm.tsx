72cdeb9290e1e63c3916ce0b7006fc7339972df2
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-04-06 13:56:12 +0100

    Mufti

--- components/EmailForm.tsx
+++ components/EmailForm.tsx
@@ -52,7 +52,7 @@ export default function EmailForm() {
             className="text-accent-500 block h-10 w-full focus:invalid:border-red-400 focus:invalid:text-red-500 focus:invalid:ring-red-500 appearance-none rounded-lg border-2 border-slate-300 px-4 py-2 placeholder-zinc-400 duration-200 focus:outline-none focus:ring-zinc-300 sm:text-sm"
             id="name"
             name="name"
-            placeholder="John Doe"
+            placeholder="Mufti Menk"
             required
             type="text"
             value={name}
@@ -67,7 +67,7 @@ export default function EmailForm() {
             pattern="[a-z0-9._%+\-]+@[a-z0-9.\-]+\.[a-z]{2,}$"
             id="email-address"
             name="email"
-            placeholder="johndoe@example.com"
+            placeholder="mufti@menk.com"
             required
             type="email"
             value={email}
