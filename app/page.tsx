2059b06ac0c1fba027275f9e82c9d5e7c825a3d9
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-04-06 06:48:16 +0200

    Barcaaa

--- app/page.tsx
+++ app/page.tsx
@@ -13,24 +13,24 @@ export default function Home() {
             src="/screenshot_explore-portrait.png"
             alt="Munch App Screenshot"
             fill
-            className="object-contain mt-8 md:mt-24 px-14 object-top"
+            className="object-contain mt-8 md:mt-24 px-14 object-bottom"
           />
-          <Image
+          {/* <Image
             src="/screenshot_account-portrait.png"
             alt="Munch App Screenshot"
             fill
             className="object-contain mt-8 md:mt-10 px-14 object-bottom"
-          />
+          /> */}
         </div>
 
         <main className="flex flex-col gap-8 mt-4 md:justify-center px-6 pb-10">
-          <p className="p-2 border-2 rounded-full font-medium text-slate-400 w-72 text-center">
+          <p className="p-2 border-gray-300 border-2 rounded-full font-medium text-gray-400 w-60 text-center text-sm">
             COMING SOON FOR LONDON
           </p>
           <h1 className="font-semibold tracking-tight text-zinc-900 text-3xl leading-tight md:text-4xl max-w-lg">
             {/* <span className="text-red-500">Find</span> + <span className="text-purple-500">share</span> the best{" "}
             <span className="text-emerald-500">حلال</span> restaurants in London */}
-            Discover the best حلال restaurants. Share with your friends.
+            Find the best حلال restaurants. Share with your friends.
           </h1>
           <p className="text-gray-500">Join the waitlist to get early access when our app is available!</p>
 
