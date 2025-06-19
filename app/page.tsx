bdef879471d412d96f489af5985642f018eb8952
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-06-19 23:52:33 +0100

    Update landing page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,9 @@
-import EmailForm from "@/components/EmailForm";
+import HalalIcon from "@/components/HalalIcon";
+import { CakeSliceIcon, FishIcon, HamburgerIcon, PizzaIcon, VeganIcon } from "lucide-react";
 import Image from "next/image";
 import { Toaster } from "react-hot-toast";
+import { FaApple, FaAndroid, FaLeaf } from "react-icons/fa";
+import { FaShrimp } from "react-icons/fa6";
 
 export default function Home() {
   return (
@@ -8,7 +11,7 @@ export default function Home() {
       <Toaster />
 
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-2">
-        <div className="md:h-full h-[400px] bg-[#E8E0FA] relative overflow-hidden">
+        <div className="md:h-full h-[400px] bg-green-100 relative overflow-hidden">
           <Image
             src="/app-mockup.png"
             alt="Munch Mobile App"
@@ -18,21 +21,46 @@ export default function Home() {
         </div>
 
         <main className="flex flex-col gap-4 md:gap-8 mt-4 md:justify-center px-6 pb-10">
-          <p className="p-2 border-gray-300 border-2 rounded-full font-medium text-gray-400 w-60 text-center text-sm">
-            🚀 COMING SOON FOR LONDON
-          </p>
+          <div className="text-gray-500 flex items-center gap-2">
+            <PizzaIcon size={40} color="#b1b1b1" />
+            {/* <FishIcon size={40} color="#b1b1b1" /> */}
+            <HamburgerIcon size={40} color="#b1b1b1" />
+            <CakeSliceIcon size={40} color="#b1b1b1" />
+            <VeganIcon size={40} color="#b1b1b1" />
+            <HalalIcon size={40} color="#b1b1b1" strokeWidth={2.5} />
+          </div>
           <h1 className="font-semibold tracking-tight text-zinc-900 text-2xl leading-tight md:text-4xl max-w-lg">
-            {/* <span className="text-red-500">Find</span> + <span className="text-purple-500">share</span> the best{" "}
-            <span className="text-emerald-500">حلال</span> restaurants in London */}
-            🔎 <span className="text-green-700">Discover</span> حلال restaurants.
+            🔎 <span className="text-green-700">Discover</span> top restaurants.
             <br />
             ❤️ <span className="text-red-700">Save</span> your favourites.
             <br />
             🌐 <span className="text-blue-700">Share</span> with friends.
+            <br />
           </h1>
-          <p className="text-gray-500">Join the waitlist to get early access when our app is available!</p>
+          <p className="text-gray-500 text-lg">
+            Join Munch <b>(beta)</b> and try now!
+          </p>
 
-          <EmailForm />
+          <div className="flex gap-2">
+            <a
+              href="https://play.google.com/store/apps/details?id=com.zafaris.munch"
+              className="p-2 border-gray-400 border-2 rounded-full font-medium text-gray-500 text-lg w-60 text-center flex items-center justify-center"
+            >
+              <div className="flex items-center gap-2">
+                <FaAndroid />
+                <span>Android</span>
+              </div>
+            </a>
+            <a
+              href="https://testflight.apple.com/join/5zm4KwMH"
+              className="p-2 border-gray-400 border-2 rounded-full font-medium text-gray-500 text-lg w-60 text-center flex items-center justify-center"
+            >
+              <div className="flex items-center gap-2">
+                <FaApple />
+                <span>iOS</span>
+              </div>
+            </a>
+          </div>
         </main>
       </section>
     </>
---
+++ app/waitlist/page.tsx
@@ -0,0 +1,40 @@
+import EmailForm from "@/components/EmailForm";
+import Image from "next/image";
+import { Toaster } from "react-hot-toast";
+
+export default function Home() {
+  return (
+    <>
+      <Toaster />
+
+      <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-2">
+        <div className="md:h-full h-[400px] bg-[#E8E0FA] relative overflow-hidden">
+          <Image
+            src="/app-mockup.png"
+            alt="Munch Mobile App"
+            fill
+            className="object-contain px-4 md:px-14 pt-10 object-bottom"
+          />
+        </div>
+
+        <main className="flex flex-col gap-4 md:gap-8 mt-4 md:justify-center px-6 pb-10">
+          <p className="p-2 border-gray-300 border-2 rounded-full font-medium text-gray-400 w-60 text-center text-sm">
+            🚀 COMING SOON FOR LONDON
+          </p>
+          <h1 className="font-semibold tracking-tight text-zinc-900 text-2xl leading-tight md:text-4xl max-w-lg">
+            {/* <span className="text-red-500">Find</span> + <span className="text-purple-500">share</span> the best{" "}
+            <span className="text-emerald-500">حلال</span> restaurants in London */}
+            🔎 <span className="text-green-700">Discover</span> حلال restaurants.
+            <br />
+            ❤️ <span className="text-red-700">Save</span> your favourites.
+            <br />
+            🌐 <span className="text-blue-700">Share</span> with friends.
+          </h1>
+          <p className="text-gray-500">Join the waitlist to get early access when our app is available!</p>
+
+          <EmailForm />
+        </main>
+      </section>
+    </>
+  );
+}
