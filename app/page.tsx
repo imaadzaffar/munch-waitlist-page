187599ea84f9bbad917928bebd8ee045d737a43e
Imaad Zaffar <56759143+imaadzaffar@users.noreply.github.com>
2025-06-21 01:57:19 +0100

    Update mockups and icons

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
 import HalalIcon from "@/components/HalalIcon";
-import { CakeSliceIcon, FishIcon, HamburgerIcon, PizzaIcon, VeganIcon } from "lucide-react";
+import {
+  BeefIcon,
+  CakeSliceIcon,
+  DrumstickIcon,
+  FishIcon,
+  ForkKnifeIcon,
+  HamburgerIcon,
+  IceCreamBowlIcon,
+  PizzaIcon,
+  SaladIcon,
+  UtensilsCrossedIcon,
+  VeganIcon,
+} from "lucide-react";
 import Image from "next/image";
 import { Toaster } from "react-hot-toast";
 import { FaApple, FaAndroid, FaLeaf } from "react-icons/fa";
@@ -13,7 +25,7 @@ export default function Home() {
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-2">
         <div className="md:h-full h-[400px] bg-green-100 relative overflow-hidden">
           <Image
-            src="/app-mockup.png"
+            src="/app-mockup-v2.png"
             alt="Munch Mobile App"
             fill
             className="object-contain px-4 md:px-14 pt-10 object-bottom"
@@ -21,23 +33,26 @@ export default function Home() {
         </div>
 
         <main className="flex flex-col gap-4 md:gap-8 mt-4 md:justify-center px-6 pb-10">
-          <div className="text-gray-500 flex items-center gap-2">
-            <PizzaIcon size={40} color="#b1b1b1" />
-            {/* <FishIcon size={40} color="#b1b1b1" /> */}
-            <HamburgerIcon size={40} color="#b1b1b1" />
-            <CakeSliceIcon size={40} color="#b1b1b1" />
-            <VeganIcon size={40} color="#b1b1b1" />
-            <HalalIcon size={40} color="#b1b1b1" strokeWidth={2.5} />
-          </div>
           <h1 className="font-semibold tracking-tight text-zinc-900 text-2xl leading-tight md:text-4xl max-w-lg">
-            🔎 <span className="text-green-700">Discover</span> top restaurants.
-            <br />
+            <div className="flex items-center gap-2">
+              🔎 <span className="text-green-700">Discover</span>
+              <HalalIcon size={30} color="rgb(21,128,61)" strokeWidth={2} /> restaurants.
+            </div>
             ❤️ <span className="text-red-700">Save</span> your favourites.
             <br />
             🌐 <span className="text-blue-700">Share</span> with friends.
             <br />
           </h1>
-          <p className="text-gray-500 text-lg">
+          <div className="text-gray-500 flex w-full items-center justify-center gap-5">
+            <UtensilsCrossedIcon size={20} color="rgb(107,114,128)" />
+            <PizzaIcon size={20} color="rgb(107,114,128)" />
+            <HamburgerIcon size={20} color="rgb(107,114,128)" />
+            <BeefIcon size={20} color="rgb(107,114,128)" />
+            <SaladIcon size={20} color="rgb(107,114,128)" />
+            <IceCreamBowlIcon size={20} color="rgb(107,114,128)" />
+            <CakeSliceIcon size={20} color="rgb(107,114,128)" />
+          </div>
+          <p className="text-gray-500">
             Join Munch <b>(beta)</b> and try now!
           </p>
 
--- app/waitlist/page.tsx
+++ app/waitlist/page.tsx
@@ -1,4 +1,18 @@
 import EmailForm from "@/components/EmailForm";
+import HalalIcon from "@/components/HalalIcon";
+import {
+  BeefIcon,
+  CakeSliceIcon,
+  DrumstickIcon,
+  FishIcon,
+  ForkKnifeIcon,
+  HamburgerIcon,
+  IceCreamBowlIcon,
+  PizzaIcon,
+  SaladIcon,
+  UtensilsCrossedIcon,
+  VeganIcon,
+} from "lucide-react";
 import Image from "next/image";
 import { Toaster } from "react-hot-toast";
 
@@ -8,9 +22,9 @@ export default function Home() {
       <Toaster />
 
       <section className="w-screen h-dvh grid grid-cols-1 md:grid-cols-2 gap-2">
-        <div className="md:h-full h-[400px] bg-[#E8E0FA] relative overflow-hidden">
+        <div className="md:h-full h-[400px] bg-green-100 relative overflow-hidden">
           <Image
-            src="/app-mockup.png"
+            src="/app-mockup-v2.png"
             alt="Munch Mobile App"
             fill
             className="object-contain px-4 md:px-14 pt-10 object-bottom"
@@ -22,14 +36,23 @@ export default function Home() {
             🚀 COMING SOON FOR LONDON
           </p>
           <h1 className="font-semibold tracking-tight text-zinc-900 text-2xl leading-tight md:text-4xl max-w-lg">
-            {/* <span className="text-red-500">Find</span> + <span className="text-purple-500">share</span> the best{" "}
-            <span className="text-emerald-500">حلال</span> restaurants in London */}
-            🔎 <span className="text-green-700">Discover</span> حلال restaurants.
-            <br />
+            <div className="flex items-center gap-2">
+              🔎 <span className="text-green-700">Discover</span>
+              <HalalIcon size={30} color="rgb(21,128,61)" strokeWidth={2} /> restaurants.
+            </div>
             ❤️ <span className="text-red-700">Save</span> your favourites.
             <br />
             🌐 <span className="text-blue-700">Share</span> with friends.
           </h1>
+          <div className="text-gray-500 flex w-full items-center justify-center gap-5">
+            <UtensilsCrossedIcon size={20} color="rgb(107,114,128)" />
+            <PizzaIcon size={20} color="rgb(107,114,128)" />
+            <HamburgerIcon size={20} color="rgb(107,114,128)" />
+            <BeefIcon size={20} color="rgb(107,114,128)" />
+            <SaladIcon size={20} color="rgb(107,114,128)" />
+            <IceCreamBowlIcon size={20} color="rgb(107,114,128)" />
+            <CakeSliceIcon size={20} color="rgb(107,114,128)" />
+          </div>
           <p className="text-gray-500">Join the waitlist to get early access when our app is available!</p>
 
           <EmailForm />
